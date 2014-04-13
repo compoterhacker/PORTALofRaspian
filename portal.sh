f1b015b2645dba150445feef15eb7509903234c9
Jaime Cochran <macfags@chronicle.su>
2014-04-13 16:48:54 -0500

    Added MAC spoofing and ssh hidden service options

--- portal.sh
+++ portal.sh
@@ -12,16 +12,40 @@
 # 99.99% credits to grugq and rednerd
 #
 
+echo "[+] PORTALofRaspbian Setup Script"
+
+if [ "$(id -u)" != "0" ]; then
+   echo "[!] Run as root." 1>&2
+   exit 1
+fi
+
+echo "[+] Installing necessary goods"
 apt-get install zsh vim htop lsof # good call on these, grugq. strace included in raspbian. I prefer http://ohmyz.sh/ ;)
 apt-get install tor dnsmasq
 
+echo "[+] Creating backup of /etc/network/interfaces -> interfaces-bak"
 cp /etc/network/interfaces /etc/network/interfaces-bak
+
+MACMSG="[+] Would you like to spoof wlan0's MAC address? (Y/n): "
+read -n1 -p "$MACMSG" allow_spoof
+echo -e "\n"
+
+case $allow_spoof in
+  [nN] ) echo "[+] Defaulting to wlan0's real MAC."
+         macaddress=$(ip link show eth0 | awk '/ether/ {print $2}')
+         ;;
+  *) macaddress=$(echo -n 00:18:4D; dd bs=1 count=3 if=/dev/random 2>/dev/null |hexdump -v -e '/1 ":%02X"')
+     echo "[+] Generated random Netgear MAC address for wlan0: $macaddress"
+     ;;
+esac
+
+echo "[+] Setting up network interfaces"
 cat > /etc/network/interfaces << __INTERFACES__
 auto lo
 
 iface lo inet loopback
 
-pre-up ifconfig wlan0 hw ether 00:00:00:00:00:00 # change wlan0's mac address
+pre-up ifconfig wlan0 hw ether $macaddress
 allow-hotplug wlan0
 iface wlan0 inet manual # set to manual by default, change to auto if you already have AP's setup.
 wpa-roam /etc/wpa_supplicant/wpa_supplicant.conf
@@ -36,11 +60,36 @@ iface eth0 inet static
   netmask 255.255.255.0
 __INTERFACES__
 
+echo "[+] Backing up /etc/tor/torrc -> torrc.bak"
 cp /etc/tor/torrc /etc/tor/torrc.bak
+
+HIDDENSERVICE="[+] Would you like to run your sshd as a hidden service? [Recommended] (Y/n): "
+read -n1 -p "$HIDDENSERVICE" ssh_hs
+echo -e "\n"
+
+case $ssh_hs in
+  [nN] ) echo "[+] Weaksauce."
+         PISSH=""
+         ;;
+  *) echo "[+] Adding sshd as a hidden service for your Raspberry Pi"
+     PISSH="HiddenServiceDir /var/lib/tor/hidden_service/"
+     PISSH="HiddenServicePort 22 127.0.0.1:22"
+
+     while read line;
+     do
+       if [[ $line == "#ListenAddress 0.0.0.0" ]]; then
+         sed -i "s/#ListenAddress 0.0.0.0/ListenAddress 127.0.0.1/" /etc/ssh/sshd_config
+       fi
+     done < /etc/ssh/sshd_config;
+     ;;
+esac
+
+echo "[+] Setting up torrc..."
 cat > /etc/tor/torrc << __TORRC__
 AllowUnverifiedNodes middle,rendezvous
 Log notice syslog
 DataDirectory /var/lib/tor
+$PISSH
 SocksPort 9050
 SocksBindAddress 127.0.0.1
 SocksBindAddress 172.16.0.1:9050
@@ -52,6 +101,7 @@ DNSPort 9053
 DNSListenAddress 172.16.0.1
 __TORRC__
 
+echo "[+] Backing up /etc/dnsmasq.conf -> dnsmasq.conf.bak"
 cp /etc/dnsmasq.conf /etc/dnsmasq.conf.bak
 cat > /etc/dnsmasq.conf << __DNSMASQ__
 bogus-priv
@@ -61,6 +111,7 @@ bind-interfaces
 dhcp-range=172.16.0.50,172.16.0.150,12h
 __DNSMASQ__
 
+echo "[+] Setting up iptables..."
 cat > /etc/network/iptables.tor.rules << __IPTABLES__
 *nat
 :PREROUTING ACCEPT [0:0]
@@ -89,7 +140,23 @@ COMMIT
 COMMIT
 __IPTABLES__
 
-/etc/init.d/tor restart
+while read line;
+do
+  if [[ $line == *iptables-restore* ]]; then
+    sed -i "s/# //" /etc/network/interfaces
+  fi
+done < /etc/network/interfaces;
+
+service tor restart
+sleep 1
+service ssh reload
+sleep 1
+
+echo "[+] Setup complete"
+if [ $PISSH != "" ] then;
+  echo "[+] Your Pi's SSH onion: $(cat /var/lib/tor/hidden_service/hostname)"
+fi
+echo "[+] Reboot, plug into eth0 and giddyup"
 
 # make sure all is gravy. If tor throws errors, either you or I did something wrong.
 # if not, reboot and enjoy.
